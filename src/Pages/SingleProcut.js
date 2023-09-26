2d843dc9d51c14ea1febc0ad60438e12f4e3e94d
Mohammed-Beltagy <justbeltagy@gmail.com>
2023-09-26 10:10:42 +0300

    save login state in localstorage

--- src/Pages/SingleProcut.js
+++ src/Pages/SingleProcut.js
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Main from "../Components/Main";
 import products from "../assets/products";
 
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -8,13 +8,15 @@ const users = [
 ];
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState("beltagy"),
-    [isLogged, setIsLogged] = useState(true);
+  const [user, setUser] = useState(localStorage.getItem("user")),
+    [isLogged, setIsLogged] = useState(JSON.parse(localStorage.getItem("islogged")) || false);
 
   const login = (username, password) => {
     if (users.some((e) => e.username === username && e.password === password)) {
       setUser(username);
       setIsLogged(true);
+      localStorage.setItem("user", username);
+      localStorage.setItem("islogged", true);
       return true;
     }
     return false;
@@ -23,6 +25,8 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     setUser(null);
     setIsLogged(false);
+    localStorage.setItem("user", null);
+    localStorage.setItem("islogged", false);
   };
 
   return <AuthContext.Provider value={{ user, login, logout, isLogged }}>{children}</AuthContext.Provider>;
